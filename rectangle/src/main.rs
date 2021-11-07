a92656b3c94194a65a31288d79a9cd9492d0d8c2
Rahul <rahkulkarni@expediagroup.com>
2021-11-06 20:07:54 -0700

    Compute area using tuples

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -1,14 +1,20 @@
 fn main() {
   
-  let width1 = 30;
-  let height1 = 50;
+  // let width1 = 30;
+  // let height1 = 50;
+  let rec1 = (30, 50);
 
   println!("The aread of rectangle is {} square pixels.", 
-           area(width1, height1));
-
+           area(rec1)
+  );
 }
 
+fn area(dim: (u32, u32)) -> u32 {
+  dim.0 * dim.1
+}
+
+/*
 fn area(width: u32, height: u32) -> u32 {
   width * height
 
-}
+}*/
