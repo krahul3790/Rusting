c1f54e0f9e3a76477087a249b66504e2cffadf90
Rahul <rahkulkarni@expediagroup.com>
2021-11-06 20:05:58 -0700

    compute rectangle area

---
+++ rectangle/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+  
+  let width1 = 30;
+  let height1 = 50;
+
+  println!("The aread of rectangle is {} square pixels.", 
+           area(width1, height1));
+
+}
+
+fn area(width: u32, height: u32) -> u32 {
+  width * height
+
+}
