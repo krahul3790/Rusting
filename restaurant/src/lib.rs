5fafd85b3fded4cd123e8c7340202abf33b90e3c
Rahul <rahkulkarni@expediagroup.com>
2021-11-09 20:37:57 -0800

    Organizing code

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,64 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
+    }
+
+}
+
+fn server_order() {}
+
+mod back_of_house {
+  
+  pub enum Appetizer {
+    Soup,
+    Salad,
+  }
+
+  pub struct Breakfast {
+    pub toast: String,
+    seasonal_fruit: String,
+  }
+
+  impl Breakfast {
+    pub fn summer(toast: &str) -> Breakfast {
+      Breakfast {
+        toast: String::from(toast),
+        seasonal_fruit: String::from("peaches"),
+      }
+    }
+  }
+
+  fn cook_order(){}
+
+  fn fix_incorrect_order() {
+    cook_order();
+    super::server_order();
+  }
+}
+
+pub fn eat_at_restaurant() {
+
+  let order1 = back_of_house::Appetizer::Soup;
+  let order2 = back_of_house::Appetizer::Salad;
+
+  // Absolute Path
+  crate::front_of_house::hosting::add_to_waitlist();
+
+  // Relative Path
+  front_of_house::hosting::add_to_waitlist();
+
+  let mut meal = back_of_house::Breakfast::summer("Rye");
+  meal.toast = String::from("Wheat");
+
+  println!("{}", meal.toast);
+}
